5e1658aeecf679772bc4c7286c9e50bfa09306f0
Camilo Monsalve Gomez <beren.monsalve@gmail.com>
2014-01-10 17:09:40 -0500

    first commit

--- brochure/brochure/settings.py
+++ brochure/brochure/settings.py
@@ -10,8 +10,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.abspath(PROJECT_DIR)
+
+from deploy import DEPLOY
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -89,9 +93,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+
+
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR + '/brochure/static/'
+if DEPLOY=="DEVELOPMENT":
+    STATIC_ROOT = BASE_DIR + '/brochure/static/'
+
+if DEPLOY == "PRODUCTION":
+    STATIC_ROOT = "/home/beren5000/webapps/brochure/brochure/brochure/brochure/static"
+    STATIC_FILES_ROOT = os.path.join(PROJECT_DIR, 'security').replace('\\', '/')
 
 TEMPLATE_DIRS = (
     BASE_DIR + '/brochure/templates/'
